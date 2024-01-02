b6981699939ab11b7f56bb3a456c0810eba83fea
lawrence <lodhiambo26@gmail.com>
2024-01-02 15:06:33 +0000

    12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+
+  const getNumberOfDepartments = (employees) => {
+    return Object.keys(employees).length;
+  };
+
+  return {
+    allEmployees,
+    getNumberOfDepartments,
+  };
+}
+
