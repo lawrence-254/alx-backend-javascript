345acd7cf4074c264e4f2043b9075de4b88b9d42
lawrence <lodhiambo26@gmail.com>
2024-01-03 21:53:38 +0000

    eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (success) {
-        resolve({status: 200, body: 'Success'});
+        resolve({ status: 200, body: 'Success' });
       } else {
         const err = new Error('The fake API is not working currently');
         reject(err);
