7cbddb5050489093aae8fc912a7c492488e42dda
lawrence <lodhiambo26@gmail.com>
2024-01-03 21:49:53 +0000

    promise 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve({status: 200, body: 'Success'});
+      } else {
+        const err = new Error('The fake API is not working currently');
+        reject(err);
+      }
+    }, 5000);
+  });
+}
