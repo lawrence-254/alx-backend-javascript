aeceafa9a1b7734edfaca8239338462903d38f99
lawrence <lodhiambo26@gmail.com>
2024-01-04 14:01:29 +0000

    eslint 100

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,17 +1,17 @@
-import Car from "./10-car.js";
+import Car from './10-car';
 
 export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-    get range() {
-        return this._range;
-    }
+  get range() {
+    return this._range;
+  }
 
-    cloneCar() {
-        // Creating a new instance of the Car class with the same attribute values
-        return new Car(this._brand, this._motor, this._color);
-    }
+  cloneCar() {
+    // Creating a new instance of the Car class with the same attribute values
+    return new Car(this._brand, this._motor, this._color);
+  }
 }
