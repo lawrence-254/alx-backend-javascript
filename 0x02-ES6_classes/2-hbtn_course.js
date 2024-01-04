a6798f11498d8c94ed9c724b7a8818ff3a3ec940
lawrence <lodhiambo26@gmail.com>
2024-01-04 10:00:01 +0000

    array confirm

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, student) {
-    if (typeof name !== 'String') {
+    if (typeof name !== 'string') {
       throw new Error('Name must be a string');
     }
-   if (typeof length !== 'number') {
-     throw new Error('Length must be a number')
-   }
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (typeof student !== 'string' && typeof student !== 'array') {
+      throw new Error('student must be array of strings');
+    }
