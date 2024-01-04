6b0378c4011d0d016e2f6b663337383ed4b13135
lawrence <lodhiambo26@gmail.com>
2024-01-04 21:22:18 +0000

    10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,23 +1,41 @@
 export default class Car {
-    constructor(brand, motor, color) {
-        this._brand = brand;
-        this._motor = motor;
-        this._color = color;
-    }
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
 
-    get brand() {
-        return this._brand;
-    }
+  get brand() {
+    return this._brand;
+  }
 
-    get motor() {
-        return this._motor;
-    }
+  set brand(value) {
+    this._brand = value;
+  }
 
-    get color() {
-        return this._color;
-    }
+  get motor() {
+    return this._motor;
+  }
 
-    cloneCar() {
-        return new Car(this._brand, this._motor, this._color);
-    }
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
 }
