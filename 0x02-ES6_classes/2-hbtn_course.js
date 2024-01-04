ac8f777436d955688fc7a1cde2a9e40de92db74d
lawrence <lodhiambo26@gmail.com>
2024-01-04 10:28:13 +0000

    class student

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,41 @@
 export default class HolbertonCourse {
-  constructor(name, length, student) {
+  constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new Error('Name must be a string');
     }
     if (typeof length !== 'number') {
       throw new Error('Length must be a number');
     }
-    if (typeof student !== 'string' && typeof student !== 'array') {
-      throw new Error('student must be array of strings');
+    if (!Array.isArray(students) || !students.every((i) => typeof i === 'string')) {
+      throw new Error('student must be array of string')
     }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  setName(newName) {
+    this._name = newName;
+  }
+
+  setLength(newLength) {
+    this._length = newLength;
+  }
+
+  setStudent(newStudent) {
+    this._students = newStdent;
+  }
+}
