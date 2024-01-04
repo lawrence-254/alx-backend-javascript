294b41b3f77d3fa9f5721a6ad3afdc24ac78f6c0
lawrence <lodhiambo26@gmail.com>
2024-01-04 19:56:13 +0000

    5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,15 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Abstract classes cannot be instantiated.');
-    }
-
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
-    this._sqft = sqft;
-
-    if (typeof this.evacuationWarningMessage !== 'function') {
+    if (new.target !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+  set sqft(value) {
+    this._sqft = value;
   }
 }
