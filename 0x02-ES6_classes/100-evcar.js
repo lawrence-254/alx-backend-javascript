fabd9867b2be227a4fb14e342655ab9b558d3bce
lawrence <lodhiambo26@gmail.com>
2024-01-04 13:56:28 +0000

    100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from "./10-car.js";
+
+export default class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
+
+    get range() {
+        return this._range;
+    }
+
+    cloneCar() {
+        // Creating a new instance of the Car class with the same attribute values
+        return new Car(this._brand, this._motor, this._color);
+    }
+}
