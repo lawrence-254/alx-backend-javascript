90b6adb14a9fdaf3b2f50cbd4d13f35a1eda8918
lawrence <lodhiambo26@gmail.com>
2024-01-04 20:09:25 +0000

    7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,7 +4,15 @@ export default class Airport {
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
   toString() {
-    return `[${this._code}]`;
+    return `[${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
   }
 }
