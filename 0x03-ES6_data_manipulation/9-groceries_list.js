cedc6cf4312d3bc546e0289b3b4cfde670ad2806
lawrence <lodhiambo26@gmail.com>
2024-01-09 11:37:49 +0000

    9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const groceriesMap = new Map([
+    ['Apples', 5],
+    ['Bananas', 8],
+    ['Milk', 2],
+    ['Bread', 1],
+    ['Eggs', 12]
+    // Add more items as needed
+  ]);
+
+  return groceriesMap;
+}
