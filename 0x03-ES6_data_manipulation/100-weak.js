dc74768de9dbdf7ff65bdf6ef51e23dfe5f05a9c
lawrence <lodhiambo26@gmail.com>
2024-01-09 12:19:52 +0000

    100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,17 @@
 const weakMap = new WeakMap();
 
-export function queryAPI(endpoint) {
+function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-    throw new Error('Endpoint load is high');
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
   }
 }
+
+export { queryAPI, weakMap };
+
