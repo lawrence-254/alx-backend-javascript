6d29788f39407ebb7bf40b038ef2a090d93541b0
lawrence <lodhiambo26@gmail.com>
2024-01-09 10:03:26 +0000

    4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(studentList, city, newGrades) {
   if (!Array.isArray(studentList)) {
     return [];
   }
-  return studentList.filter((s) => s.location === city)).map((s) => {
+  return studentList.filter((s) => s.location === city).map((s) => {
     const gradeObject = newGrades.find((grade) => grade.studentId === s.id);
     const newGrade = gradeObject ? gradeObject.grade : 'N/A';
     return {
