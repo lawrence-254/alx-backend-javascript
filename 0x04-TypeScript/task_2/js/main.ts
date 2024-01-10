0c527af9ace78782f2cbdfc9feded323bf9786a2
lawrence <lodhiambo26@gmail.com>
2024-01-10 11:00:22 +0000

    task 7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -54,4 +54,15 @@ export function executeWork(employee: (Director | Teacher)) {
     return (employee as Director).workDirectorTasks();
   }
   return (employee as Teacher).workTeacherTasks();
+  }
+
+export type Subjects = ('Math' | 'History');
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
 }
