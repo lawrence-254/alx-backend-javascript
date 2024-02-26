af77999873c975376d85fc9d196e999c5da97b2d
lawrence-254 <lodhiambo26@gmail.com>
2024-02-26 18:35:20 +0000

    2try3

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 
-/*
- * Using the database database.csv (provided in project description), create a function countStudents in the file 2-read_file.js
- * Create a function named countStudents. It should accept a path in argument
- * The script should attempt to read the database file synchronously
- * If the database is not available, it should throw an error with the text Cannot load the database
- * If the database is available, it should log the following message to the console Number of students: NUMBER_OF_STUDENTS
- * It should log the number of students in each field, and the list with the following format: Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
- * CSV file can contain empty lines (at the end) - and they are not a valid student!
-*/
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
