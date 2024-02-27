2b8c9b64b32d386cb521fe7ff3a08dc6668e76a8
lawrence-254 <lodhiambo26@gmail.com>
2024-02-27 09:55:44 +0000

    4try0

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
 const HTTP = require('http');
+
 const app = HTTP.createServer();
 const PORT = 1245;
 const HOST = 'localhost';
 
 app.on('request', (_, res) => {
   const plainText = "Hello Holberton School!";
+
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', plainText.length);
   res.statusCode = 200;
