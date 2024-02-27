2ba6503c4e6050406049e6591068414ee6163ea0
lawrence-254 <lodhiambo26@gmail.com>
2024-02-27 09:52:59 +0000

    4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+const HTTP = require('http');
+const app = HTTP.createServer();
+const PORT = 1245;
+const HOST = 'localhost';
+
+app.on('request', (_, res) => {
+  const plainText = "Hello Holberton School!";
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', plainText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(plainText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
